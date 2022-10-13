edb61812e34d8c62501248830d9c4660443cfbb1
safislam1996 <islam.safidev98@gmail.com>
2022-10-13 12:58:29 +0600

    changed index.js file

--- index.js
+++ index.js
@@ -6,122 +6,91 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 const url='https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/od-+2014/q-actros?search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at+%3Adesc'
+const response = await axios.get(url)
+const $ = load(response.data)
+const total_pages=7
+let page_counter=0
+let articles=[]
 
-axios(url).then(
-    res=>{
-        const data=res.data
-        const $=load(data)
-        
-        function getNextPageUrl() {
-            const ul_list = $('ul.pagination-list li')
-            let currentPage = 1
-            const url_list = []
-            ul_list.map((idx, elm) => {
-
-                url_list.push(url.concat('&page=', currentPage))
-                currentPage++
+function addItems(val) {
             
-                
-            })
-            return url_list
-        }
-        // id:6100411379
-        // url: https://www.otomoto.pl/oferta/mercedes-benz-actros-ID6EQHFW.html
-
+    $('main article').map((idx, elm) => {
+        const item_id = $(elm).attr('id')
+        const item_url = $(elm).find('a').attr('href')
         
-        const pages=getNextPageUrl()
-        
-        console.log(pages)
-        
-        // addItems()
-        const articles=[]
-        function addItems() {
+        const article = {
+            'item_id': item_id,
+            'url': item_url,
             
-            $('main article').map((idx, elm) => {
-                const item_id = $(elm).attr('id')
-                const item_url = $(elm).find('a').attr('href')
-
-                const article = {
-                    'item_id': item_id,
-                    'url': item_url
-                }
-                // console.log(article)
-                return articles.push(article)
-                
-            })
         }
-        function getTotalAdsCount(val) {
-            console.log(val.length)
-        }
+        articles.push(article)
+        adCountLength(article['item_id'])
+        scrapeItem(article['url'])
         
-        for(let i=0;i<pages.length;i++){
-            addItems()
-            console.log("enter this for loop")
-            console.log(articles)
-        }
+    })
+}
+let adCountLength=(item)=>{
+    console.log(item.length)
+}
 
-        function scrapeTruckItem(value){
-            let url=value.map(get_url)
-            function get_url(item){
-                return item.url
-            }
-            function getEachItem(){
-                const truck_id=$('div.offer_meta span span').attr('id')
-                // console.log(truck_id)
-            }
+// Scrape the item information about the product for each page
+let scrapeItem =async (url)=>{
+    const res=await axios.get(url)
+    const $=load(res.data)
+    const truck_datas=[]
+    const truck_id=$('#ad_id').text()
+    // console.log(truck_id)
+    const title=$('span.offer-title').text().trim()
+    const price=$('span.offer-price__number').text().slice(0,8)
+    const registration_date=$('span.offer-meta__value').text().slice(7,27)
+    const span_offer=$('span.offer-main-params__item').text().replace(/ /gi,'');
+    const production_date=span_offer.slice(0,5)
+    const mileage=span_offer.slice(6,12)
+    const power=span_offer.slice(1,21)
 
-           
-        // }
-
-        function scrapeItem(url){
-            axios(url).then(
-                res=>{
-                    const data=res.data
-                    const $=load(data)
-                    const truck_id=$('#ad_id').text()
-                    const title=$('span.offer-title').text().trim()
-                    const price=$('span.offer-price__number').text().slice(0,8)
-                    const registration_date=$('span.offer-meta__value').text().slice(7,27)
-                    const span_offer=$('span.offer-main-params__item').text().replace(/ /gi,'');
-                    const production_date=span_offer.slice(0,5)
-                    const mileage=span_offer.slice(6,12)
-                    const power=span_offer.slice(1,21)
-
-                    const truck_data={
-                        'truck id':truck_id,
-                        'title':title,
-                        'price':price,
-                        'registration date':registration_date,
-                        'production date':production_date,
-                        'mileage':mileage,
-                        'power':power
-                    }
-                    
-                    // console.log(truck_data)
-
-        }).catch(err=>console.log(err))
+    const truck_data={
+        'truck id':truck_id,
+        'title':title,
+        'price':price,
+        'registration date':registration_date,
+        'production date':production_date,
+        'mileage':mileage,
+        'power':power
     }
-    scrapeItem('https://www.otomoto.pl/oferta/mercedes-benz-actros-1848-z-hidraulika-ID6EV9XH.html')
-        // let val=scrapeTruckItem(articles)
-        // console.log(val)
-        // articles.map(scrapeTruckItem)
-        
-        
 
-
-
-        
-       
-      
-        
-
-    }
     
-}).catch(err => console.error(err))
+console.log(truck_data)
 
 
 
 
+}
+// scrapeItem('https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/od-+2014/q-actros?search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at+%3Adesc')
+ 
+const getWebsiteContent = async (url) => {
+
+    try {
+        
+        const nextPageLink=$('ul.pagination-list li').find('a').attr('href')
+
+        //TODO: Add map function
+        for(let page_counter=1;page_counter<=total_pages;page_counter++){
+            const page_link=url+nextPageLink+'&page='+page_counter
+
+            addItems(page_link)
+        }
+        
+   
+        
+}
+
+    catch (error) {
+        exportResults(parsedResults)
+        console.error(error)
+      }
+}
+
+getWebsiteContent(url)
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
